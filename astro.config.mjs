0fe4cddb57fc9b4b4661b3584da1d262e2dae936
Gaston <gastonhoyo@gmail.com>
2026-03-04 17:21:32 -0300

    un mensaje descriptivo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://chieffin.com',
-  base: '/Calculadoras', // <-- QUE COINCIDA CON EL NOMBRE DEL REPO NUEVO
-  integrations: [tailwind(), sitemap()],
+  // Si apuntas el dominio a la raíz del repo, usa '/'
+  base: '/', 
+  integrations: [
+    tailwind(), 
+    sitemap()
+  ],
 });
