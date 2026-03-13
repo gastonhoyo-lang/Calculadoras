d710028e4531b365a6baac4f0783d7b09cf40af1
Gaston <gastonhoyo@gmail.com>
2026-03-12 21:04:19 -0300

    un mensaje descriptivo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://chieffin.com',
-  // Si apuntas el dominio a la raíz del repo, usa '/'
-  base: '/', 
-  integrations: [
-    tailwind(), 
-    sitemap()
-    
-  ],
+  // En las versiones nuevas, Astro detecta el modo según el adaptador.
+  // No pongas 'output' por ahora para que use el default compatible.
+  adapter: cloudflare(),
+  integrations: [tailwind()],
+  
+  // Optimizamos para que el build sea ligero
+  build: {
+    format: 'directory'
+  }
 });
