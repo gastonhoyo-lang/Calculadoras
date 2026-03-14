0b28d8413b1471d5d7243028a859848d629bd9e3
Gaston <gastonhoyo@gmail.com>
2026-03-14 19:34:25 -0300

    un mensaje descriptivo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // 'server' es obligatorio para que el [slug].astro pueda consultar el KV en tiempo real
   output: 'server',
-  
   adapter: cloudflare({
-    // Usamos el servicio de imágenes nativo de Cloudflare
     imageService: 'cloudflare',
-    platformProxy: {
-      enabled: true,
-    },
+    platformProxy: { enabled: true },
   }),
-
   integrations: [tailwind()],
-
-  // Dejamos que Astro maneje la base automáticamente
   base: '/',
-  
-  // Opcional: Esto ayuda a que las URLs sean más limpias 
-  // (chieffin.com/utiles/slug en lugar de slug.html)
-  trailingSlash: 'always',
+  trailingSlash: 'never', // <--- CAMBIA ESTO A 'never'
 });
